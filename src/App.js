47df070b032c2cb3e7ec44b9c0c9a6915f920bf0
dnyandeoShekade <shekadednyande@gmail.com>
2024-09-17 14:48:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BootstraoDemoPage from './BootstraoDemoPage';
+import BasicExample from './componet/Cards';
+// import NavScrollExample from './'
+import IndividualIntervalsExample from './componet/Carosouel';
+import ControlledCarousel from './componet/Carosouel';
+import Navbar from './componet/Navbar'
+// import NavScrollExample from '.';
+import Vacation from'./componet/Vacation';
+import PetCareSection from './componet/PetCareSection';
+import PetServices from './componet/PetServices';
+import SpecialCareOnPets from './componet/SpecialCareOnPets';
+import Pricecard from './componet/Pricecard';
+import ImgCard from './componet/ImgCard';
+import Client  from './componet/Client';
+import PetBio from './componet/PetBio';
+import Footer from './componet/Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/* <BootstraoDemoPage/>  */}
+      {/* <ResponsiveExample/>*/}
+      {/* <ControlledCarousel/>  */}
+      {/* <BasicExample/> */}
+      {/* <NavScrollExample/>  */}
+      <Navbar/>
+      <IndividualIntervalsExample/>
+      <Vacation/>
+      <PetCareSection/>
+      <PetServices/>
+      <SpecialCareOnPets/>
+      <Pricecard/>
+      <ImgCard/>
+      <Client/>
+      <PetBio/>
+      <Footer/>
+     
     </div>
   );
 }
